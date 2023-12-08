d2fa9ea556d995af4db5909df44fb24dc3553b95
maxcks <maxcks93@gmail.com>
2023-12-09 00:44:13 +0800

    Able to search product from Spotify and save playlist to Spotify

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,71 +4,63 @@ import './App.css';
 import SearchBar from '../SearchBar/SearchBar';
 import SearchResult from '../SearchResult/SearchResult';
 import Playlist from '../Playlist/Playlist';
+import Spotify from '../../util/Spotify';
 
 function App() {
 
   //Hardcode an array of song for testing
-  const songs = [
-    {
-      id: 1,
-      name: 'Yellow',
-      artist: 'Coldplay',
-      album: 'Parachutes'
-    },
-    {
-      id: 2,
-      name: 'Bad Day',
-      artist: 'Daniel Powter',
-      album: 'Daniel Powter'
-    },
-    {
-      id: 3,
-      name: 'A Thousand Year',
-      artist: 'Christina Perri',
-      album: 'The Twilight Saga'
-    },
-    {
-      id: 4,
-      name: 'Baby',
-      artist: 'Justin Bieber',
-      album: 'Baby'
-    },
-    {
-      id: 5,
-      name: 'My Heart Will Go On',
-      artist: 'Christina Perri',
-      album: 'Titanic'
-    }
-  ];
-
-  const playlistName = 'Max Cks';
+  // const songs = [
+  //   {
+  //     id: 1,
+  //     name: 'Yellow',
+  //     artist: 'Coldplay',
+  //     album: 'Parachutes'
+  //   },
+  //   {
+  //     id: 2,
+  //     name: 'Bad Day',
+  //     artist: 'Daniel Powter',
+  //     album: 'Daniel Powter'
+  //   },
+  //   {
+  //     id: 3,
+  //     name: 'A Thousand Year',
+  //     artist: 'Christina Perri',
+  //     album: 'The Twilight Saga'
+  //   },
+  //   {
+  //     id: 4,
+  //     name: 'Baby',
+  //     artist: 'Justin Bieber',
+  //     album: 'Baby'
+  //   },
+  //   {
+  //     id: 5,
+  //     name: 'My Heart Will Go On',
+  //     artist: 'Christina Perri',
+  //     album: 'Titanic'
+  //   }
+  // ];
 
   // State hook to store search result array
   const [searchResults, setSearchResults] = useState([]);
   // State hook to store the tracks in the playlistTracks array
-  const [playlistTracks, setPlaylistTracks] = useState([
-    {
-      id: 1,
-      name: 'Yellow',
-      artist: 'Coldplay',
-      album: 'Parachutes'
-    },
-    {
-      id: 2,
-      name: 'Bad Day',
-      artist: 'Daniel Powter',
-      album: 'Daniel Powter'
-    }
-  ]);
+  const [playlistTracks, setPlaylistTracks] = useState([]);
+  // State hook for playlistName to provide customization to user
+  const [playlistName, setPlaylistName] = useState('New Playlist')
 
   // search track function
-  const searchTrack = useCallback((searchName)=>{
-    const foundSong = songs.find(song => song.name === searchName);
+  // const searchTrack = useCallback((searchName)=>{
+  //   const foundSong = songs.find(song => song.name === searchName);
+  //   setSearchResults((originalData) => {
+  //     return [...originalData, foundSong];
+  //   });
+  // },[]);
 
-    setSearchResults((originalData) => {
-      return [...originalData, foundSong];
-    });
-  },[]);
+  // function to search track in Spotify via track's name
+  const searchTrack = useCallback((term) => {
+    Spotify.search(term).then(setSearchResults);
+  }, []);
 
   // function to add track from search result to playlist
   const addTrack = useCallback((track)=>{
@@ -87,8 +79,19 @@ function App() {
   );
   },[]);
 
- // for testing purpose
- console.log(searchResults);
+  // function to rename the playlist
+  const renamePlaylist = useCallback((name)=>{
+    setPlaylistName(name);
+  },[])
+
+  // function to save the playlist to the user's spotify account
+  const savePlaylist = useCallback(() => {
+    const trackUris = playlistTracks.map((track) => track.uri);
+    Spotify.savePlaylist(playlistName, trackUris).then(() => {
+      setPlaylistName('New Playlist');
+      setPlaylistTracks([]);
+    });
+  }, [playlistName, playlistTracks]);
 
   return (
     <div className="App">
@@ -100,7 +103,9 @@ function App() {
       <Playlist 
       playlistName = {playlistName}
       playlistTracks = {playlistTracks}
-      onRemove={removeTrack}/>
+      onRemove={removeTrack}
+      onRename={renamePlaylist}
+      onSave={savePlaylist}/>
     </div>
   );
 }
--- src/components/Playlist/Playlist.js
+++ src/components/Playlist/Playlist.js
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import './Playlist.css';
 import Tracklist from '../Tracklist/Tracklist';
 
 function Playlist(props){
 
+    // invoke the renamePlaylist function of the root component.
+    // the function will take in the value as the user input
+    const handleNameChange = useCallback((event)=>{
+        props.onRename(event.target.value);
+    },[props.onRename]);
+
     return (
         <div className='Playlist'>
-            <h2>{props.playlistName}</h2>
+            <input 
+            onChange={handleNameChange}
+            value={props.playlistName}
+            />
             <Tracklist 
             tracks={props.playlistTracks}
             isSaved={true}
             onRemove={props.onRemove}/>
-            <button>
+            <button onClick={props.onSave}>
                 SAVE TO SPOTIFY
             </button>
         </div>
