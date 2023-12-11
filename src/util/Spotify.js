96cece87360dc4d7516cfc7002a7528d27afef8b
maxcks <maxcks93@gmail.com>
2023-12-11 11:54:02 +0800

    Add some comments

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -5,21 +5,35 @@ let accessToken;
 
 const Spotify = {
   async getAccessToken() {
+    // Check if the access token is already available
+    if (accessToken) {
+      return accessToken;
+    }
+
+      // To extract access token and expiration time from the URL
       const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
       const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
 
+      // If access token and expiration time are present in the URL
       if (accessTokenMatch && expiresInMatch) {
         accessToken = accessTokenMatch[1];
         const expiresIn = Number(expiresInMatch[1]);
+
+        // Calculate the expiration time and store the token in localStorage
         const expirationTime = Date.now() + expiresIn * 1000;
         localStorage.setItem('accessToken', accessToken);
         localStorage.setItem('tokenExpiration', expirationTime);
+
+        // Clear the URL parameters
         window.history.pushState('Access Token', null, '/');
       } else {
+        // If no access token in the URL, check localStorage for a stored token
         const storedToken = localStorage.getItem('accessToken');
         const tokenExpiration = localStorage.getItem('tokenExpiration');
 
+        // If no stored token or the stored token is expired
         if (!storedToken || Date.now() > tokenExpiration) {
+          // Redirect to Spotify authorization URL
           const accessUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectUri}`;
           window.location = accessUrl;
         }
@@ -30,6 +44,7 @@ const Spotify = {
     return accessToken;
   },
 
+  //function to search track from spotify
   async search(term) {
       const accessToken = await Spotify.getAccessToken();
       const response = await fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`, {
@@ -53,6 +68,7 @@ const Spotify = {
       }));
   },
 
+  //function to create playlist and save the track into playlist into spotify's account
   async savePlaylist(name, trackUris) {
       if (!name || !trackUris.length) {
         return;
